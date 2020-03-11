e8c2d74935cbdbac417f8811b8d289f6b3295a86
Zhaoxiaotong <xiaotong.zhao@britishcouncil.org.cn>
2020-03-11 15:15:32 +0800

    change environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   hmr: false,
   remoteServiceBaseUrl: "https://localhost:5050",
-  tokenServiceBaseUrl: "https://localhost:5060"
+  tokenServiceBaseUrl: "http://47.96.25.72:5060"
 };
